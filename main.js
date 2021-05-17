dbcc9dbb4d7c8bd3e3948e609d12495cadac9ef5
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-17 12:32:26 +0200

    dev: added number text

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 console.log("toto");
 
-function draw() {
-  console.log("start draw");
+const n = 15;
+
+function drawCircles() {
+  console.log("start draw circle");
   const g = document.querySelector("svg.multi-svg g.points");
   if (!g) {
     throw new Error("svg not found.");
@@ -12,8 +14,8 @@ function draw() {
   const y = 0;
   const r = 100;
 
-  for (let i = 0; i < 10; i++) {
-    const angleDegree = (i * 360) / 10;
+  for (let i = 0; i < n; i++) {
+    const angleDegree = (i * 360) / n;
     const angleRad = (angleDegree * Math.PI) / 180;
     const cx = x + r * Math.sin(angleRad);
     const cy = y + r * Math.cos(angleRad);
@@ -28,4 +30,34 @@ function draw() {
   }
 }
 
-draw();
+function drawNumbers() {
+  console.log("start draw numbers");
+  const g = document.querySelector("svg.multi-svg g.numbers");
+  if (!g) {
+    throw new Error("svg not found.");
+  }
+
+  // big circle attributes.
+  const x = -5;
+  const y = -5;
+  const r = 100 + 10;
+
+  for (let i = 0; i < n; i++) {
+    const angleDegree = (i * 360) / n;
+    const angleRad = (angleDegree * Math.PI) / 180;
+    const cx = x + r * Math.sin(angleRad);
+    const cy = y + r * Math.cos(angleRad);
+    const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    group.setAttribute("transform", `translate(${cx},${cy})`);
+    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    text.setAttribute("x", 0);
+    text.setAttribute("y", 0);
+    text.setAttribute("transform", "scale(1,-1)");
+    text.textContent = "" + i;
+    group.appendChild(text);
+    g.appendChild(group);
+  }
+}
+
+drawCircles();
+drawNumbers();
