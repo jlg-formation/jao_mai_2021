b5b7e6e51372eb74bf795fe7f31b5057ca3ebe64
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-17 14:18:40 +0200

    dev: added lines

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log("toto");
 
-const n = 15;
+const n = 10;
+const multi = 2;
 
 function drawCircles() {
   console.log("start draw circle");
@@ -59,5 +60,36 @@ function drawNumbers() {
   }
 }
 
+function drawLines() {
+  console.log("start draw numbers");
+  const g = document.querySelector("svg.multi-svg g.lines");
+  if (!g) {
+    throw new Error("svg not found.");
+  }
+
+  // big circle attributes.
+  const x = 0;
+  const y = 0;
+  const r = 100;
+
+  for (let i = 0; i < n; i++) {
+    const startAngleDegree = (i * 360) / n;
+    const startAngleRad = (startAngleDegree * Math.PI) / 180;
+    const endAngleDegree = startAngleDegree * multi;
+    const endAngleRad = (endAngleDegree * Math.PI) / 180;
+    const x1 = x + r * Math.sin(startAngleRad);
+    const y1 = y + r * Math.cos(startAngleRad);
+    const x2 = x + r * Math.sin(endAngleRad);
+    const y2 = y + r * Math.cos(endAngleRad);
+    const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute("x1", x1);
+    line.setAttribute("y1", y1);
+    line.setAttribute("x2", x2);
+    line.setAttribute("y2", y2);
+    g.appendChild(line);
+  }
+}
+
 drawCircles();
 drawNumbers();
+drawLines();
