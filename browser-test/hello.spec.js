bdbf6a01b8ba5a1bf3b123ec401df352482da9a7
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-19 10:20:41 +0200

    test: added browser test

---
+++ browser-test/hello.spec.js
@@ -0,0 +1,12 @@
+const assert = chai.assert;
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('titi', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('toto', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
